472dffd1bf120511aae89f3f3ce3f903d596111d
Ben Schwartz <benschw@gmail.com>
2013-04-06 23:58:21 -0500

    mongo

--- mongo.php
+++ mongo.php
@@ -22,11 +22,11 @@ try {
 				'userId' 		 => 5,
 				'startPrice'	 => 0.01,
 				'currentPrice'	 => 0.02,
-				'orgName'        => 'Austin Pets Alive',
+				'orgName'        => 'Austin Dog Rescue',
            		'marketName'     => '$25 Gift Cards to Local Resturants',
             	'description'    => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
             	'orgImage'       => '/images/logos/austindog.png',
-            	'seoName'        => 'austin-pets-alive-gift-cards'
+            	'seoName'        => 'austin-dog-rescue-gift-cards'
 	));
 	$db->Market->insert(array(
 				'id' => 2,
